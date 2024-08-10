a2bbb087e6684321abdfb5db265fb8ffdd9c250a
Rishabh Jambhulkar <88210544+rishabhjambhulkar@users.noreply.github.com>
2024-08-10 16:21:02 +0530

    completed EMS system

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,31 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
-import EmployeeManagementApp from './pages/EmployeeList/EmployeeManagementApp';
-import EmployeeDetails from './pages/EmployeeList/EmployeeDetails';
-
 import { useState } from 'react';
+import Dashboard from './pages/Dashboard/Dashboard';
+import EmployeeManagementApp from './pages/'; // Assuming you have these imports
+import EmployeeDetails from './pages/EmployeeDetails'; // Assuming you have these imports
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   console.log(isAuthenticated);
+
   return (
     <BrowserRouter>
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
-       
-       
-        <Route path="/employee" element={<EmployeeManagementApp />} />
-        <Route path="/employee/:id" element={<EmployeeDetails />} />
-
         <Route path='/sign-in' element={<SignIn setIsAuthenticated={setIsAuthenticated} />} />
         <Route path='/sign-up' element={<SignUp />} />
         <Route element={<PrivateRoute setIsAuthenticated={setIsAuthenticated} isAuthenticated={isAuthenticated} />}>
           <Route path='/profile' element={<Profile setIsAuthenticated={setIsAuthenticated} />} />
-          {/* <Route path="/employee" element={<EmployeeManagementApp  />} />
-          <Route path="/employee/:id" element={<EmployeeDetails />} /> */}
+          <Route path='/employee' element={<EmployeeManagementApp />} />
+          <Route path='/employee/:id' element={<EmployeeDetails />} />
+          <Route path='/dashboard' element={<Dashboard />} />
         </Route>
       </Routes>
     </BrowserRouter>
