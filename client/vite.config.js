110a9a4532cddeda2f9ee4548fddb1ba983d333b
Rishabh Jambhulkar <88210544+rishabhjambhulkar@users.noreply.github.com>
2024-08-13 16:41:08 +0530

    vite modify

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,3 +1,20 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:3000',
+//         secure: false,
+//       },
+//     },
+//   },
+//   plugins: [react()],
+// });
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
@@ -6,7 +23,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://ems-cbf9rakmc-rishabh-jambhulkars-projects.vercel.app',
+        changeOrigin: true,
         secure: false,
       },
     },
